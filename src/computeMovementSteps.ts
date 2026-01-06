5254c51f2747b35c2e3af983fa007b8996a727b9
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-01-05 18:49:22 -0800

    fix bug with computation of movement distance

--- src/computeMovementSteps.ts
+++ src/computeMovementSteps.ts
@@ -10,12 +10,20 @@ const countSpacesBetweenIslands = (
     fromIslandNumber: number,
     toIslandNumber: number,
 ) => {
+    // find the indices of the islands
     const fromIndex = islands.findIndex((island) => {
         return island.islandNumber === fromIslandNumber;
     });
     const toIndex = islands.findIndex((island) => {
         return island.islandNumber === toIslandNumber;
     });
+
+    // if either island is not found, then just assume the distance is 0
+    if (fromIndex === -1 || toIndex === -1) {
+        return 0;
+    }
+
+    // use the minimum distance between the two islands
     return Math.min(
         Math.abs(fromIndex - toIndex + islands.length) % islands.length,
         Math.abs(fromIndex - toIndex - islands.length) % islands.length,
