34aa1842911b9f0dfcd4540687cf606a2438c443
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-01-08 23:03:47 -0800

    add gradient background to message log

--- src/messageLog.tsx
+++ src/messageLog.tsx
@@ -12,11 +12,22 @@ export const MessageLog = (props: { readonly gameState: GameSerialized }) => {
             style={{
                 border: '1px solid',
                 height: '100%',
+                maxHeight: '80vh',
                 minWidth: '200px',
                 overflowY: 'auto',
                 padding: '4px',
             }}
         >
+            <div
+                style={{
+                    background:
+                        'linear-gradient(180deg, #00000044 0%, #FFFFFF00 100%)',
+                    height: '80vh',
+                    position: 'sticky',
+                    top: '-10px',
+                    width: '100%',
+                }}
+            />
             {props.gameState.messages.map((message, index) => {
                 return (
                     // eslint-disable-next-line react/no-array-index-key
