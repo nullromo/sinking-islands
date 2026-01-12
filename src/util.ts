214615dc938d27fc26f83d642ac005b731747cd5
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-01-12 14:02:09 -0800

    add utility type

--- src/util.ts
+++ src/util.ts
@@ -50,3 +50,15 @@ export const upperSnakeToTitle = (message: string) => {
         })
         .join(' ');
 };
+
+export type MakeEmptyKeysOptional<T> = {
+    [K in keyof T]-?: (
+        x: Record<string, never> extends T[K]
+            ? Partial<Record<K, T[K]>>
+            : Record<K, T[K]>,
+    ) => void;
+}[keyof T] extends (x: infer I) => void
+    ? I extends infer U
+        ? { [K in keyof U]: U[K] }
+        : never
+    : never;
