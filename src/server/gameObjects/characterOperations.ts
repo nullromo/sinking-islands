61c48d0d742aaf768e862da52ed319a39e1cccc2
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-01-15 12:56:59 -0800

    create operations files, moving away from OOP and towards C-like operational functions

---
+++ src/server/gameObjects/characterOperations.ts
@@ -0,0 +1,33 @@
+import type { CharacterSerialized, PlayerDesignator } from '../../commonTypes';
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace CharacterOperations {
+    /**
+     * Creates a new character.
+     */
+    export const create = (
+        playerDesignator: PlayerDesignator,
+        strength: number,
+    ): CharacterSerialized => {
+        return {
+            playerDesignator,
+            strength,
+            tortoise: false,
+        };
+    };
+
+    /**
+     * Tells if two characters are equal to each other.
+     */
+    export const equals = (
+        character: CharacterSerialized,
+        other?: CharacterSerialized | null,
+    ) => {
+        return (
+            other &&
+            character.playerDesignator === other.playerDesignator &&
+            character.strength === other.strength &&
+            character.tortoise === other.tortoise
+        );
+    };
+}
