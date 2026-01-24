8083fa28033d8d8062508b9fef40a7e39e99f537
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-01-23 16:36:18 -0800

    migrate to vite

---
+++ vite.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from '@nabla/vite-plugin-eslint';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+//import richSvg from 'vite-plugin-react-rich-svg';
+
+export default defineConfig(() => {
+    const proxyTarget = 'http://127.0.0.1:5321';
+    return {
+        // eslint-disable-next-line no-undef
+        build: { outDir: process.env.VITE_BUILD_PATH ?? 'build' },
+        plugins: [
+            react(),
+            eslint(),
+            nodePolyfills({ include: ['util'] }),
+            //richSvg(),
+        ],
+        server: {
+            port: 3000,
+            proxy: {
+                '/backend': { changeOrigin: true, target: proxyTarget },
+                '/socket.io': {
+                    changeOrigin: true,
+                    target: proxyTarget,
+                    ws: true,
+                },
+            },
+        },
+    };
+});
