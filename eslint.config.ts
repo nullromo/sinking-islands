bb605e496dde1667414c688ef37800403b827869
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-01-23 22:17:58 -0800

    fix eslint maybe

--- eslint.config.ts
+++ eslint.config.ts
@@ -27,7 +27,10 @@ export default defineConfig([
         languageOptions: {
             ecmaVersion: 'latest',
             parser: eslintTS.parser,
-            parserOptions: { ecmaFeatures: { jsx: true } },
+            parserOptions: {
+                ecmaFeatures: { jsx: true },
+                project: './tsconfig.json',
+            },
             sourceType: 'module',
         },
         plugins: {
