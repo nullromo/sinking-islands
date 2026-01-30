df9ba0de17392d655e3bbae61db3347d5022d342
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-01-30 15:52:07 -0800

    fix jest config to work with uuid package

--- jest.config.ts
+++ jest.config.ts
@@ -180,10 +180,7 @@ const config: Config = {
     // transform: undefined,
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "/node_modules/",
-    //   "\\.pnp\\.[^\\/]+$"
-    // ],
+    transformIgnorePatterns: ['node_modules/(?!(uuid)/)'],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
