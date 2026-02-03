423e14e561a9cdc837333a17f5d127d7a12a8b84
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-02-02 22:32:40 -0800

    migrate movement handler

--- src/server/gameObjects/gameOperations.ts
+++ src/server/gameObjects/gameOperations.ts
@@ -9,6 +9,7 @@ import {
     otherPlayerDesignator,
     PlayerDesignator,
 } from '../../commonTypes';
+import { computeMovementSteps } from '../../computeMovementSteps';
 import { createBlankGame } from '../../createBlankGame';
 import { GameActionType, type GameAction } from '../../gameActionTypes';
 import { GameState } from '../../gameState';
@@ -20,7 +21,11 @@ import { ActionOrderTrackOperations } from './actionOrderTrackOperations';
 import { CardType } from './card';
 import { CharacterOperations } from './characterOperations';
 import { IslandOperations } from './islandOperations';
-import type { FlyingFishMovement, TargetCharacter } from './player';
+import type {
+    FlyingFishMovement,
+    MovementSet,
+    TargetCharacter,
+} from './player';
 import { PlayerOperations } from './playerOperations';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -661,6 +666,204 @@ export namespace GameOperations {
         IslandOperations.removeCharacter(targetIsland, harpoonTarget.character);
     };
 
+    const handleMovementAction = (
+        game: GameSerialized,
+        playerDesignator: PlayerDesignator,
+        movementSet: MovementSet,
+    ) => {
+        // there must be at least one movement
+        if (movementSet.length < 1) {
+            throw new Error('Must include at least 1 movement.');
+        }
+
+        // check all movements
+        for (const movement of movementSet) {
+            // the player must move their own character
+            if (movement.character.playerDesignator !== playerDesignator) {
+                throw new Error("Cannot move a character that isn't yours.");
+            }
+
+            const toIsland = findIsland(game, movement.toIslandNumber);
+
+            // can't move to an island that already sank
+            if (!toIsland) {
+                throw new Error("Cannot move to an island that doesn't exist.");
+            }
+
+            const fromIsland = findIsland(game, movement.fromIslandNumber);
+
+            // can't move from an island that doesn't exist
+            if (!fromIsland) {
+                throw new Error(
+                    "Cannot move from an island that doesn't exist.",
+                );
+            }
+
+            // can't move a character that is not there
+            if (
+                !IslandOperations.findCharacter(fromIsland, movement.character)
+            ) {
+                throw new Error("Cannot move a character that isn't there.");
+            }
+
+            // can't move to a netted island
+            if (islandIsNetted(game, movement.toIslandNumber)) {
+                throw new Error('Cannot move to a netted island.');
+            }
+
+            // can't move off a netted island
+            if (islandIsNetted(game, movement.fromIslandNumber)) {
+                throw new Error('Cannot move off a netted island.');
+            }
+
+            // movements cannot start and end on the same island
+            if (movement.fromIslandNumber === movement.toIslandNumber) {
+                throw new Error(
+                    'Cannot move from an island to the same island.',
+                );
+            }
+        }
+
+        // the positioning of the characters after the movement must be legal
+        for (const island of game.islands) {
+            // if the island is not a small capacity island or if it has
+            // pilings, then there won't be a problem
+            if (
+                !island.smallCapacity ||
+                islandHasPilings(game, island.islandNumber)
+            ) {
+                continue;
+            }
+
+            // find out how many characters were added to the island
+            const numberOfCharactersThatLeft = movementSet.reduce(
+                (total, movement) => {
+                    return (
+                        total +
+                        (movement.fromIslandNumber === island.islandNumber
+                            ? 1
+                            : 0)
+                    );
+                },
+                0,
+            );
+
+            // find out how many characters were removed from the island
+            const numberOfCharactersThatArrived = movementSet.reduce(
+                (total, movement) => {
+                    return (
+                        total +
+                        (movement.toIslandNumber === island.islandNumber
+                            ? 1
+                            : 0)
+                    );
+                },
+                0,
+            );
+
+            // determine how many characters will be left on the island
+            const numberOfCharactersAfterMovement =
+                island.characters.length +
+                numberOfCharactersThatArrived -
+                numberOfCharactersThatLeft;
+
+            // make sure the number of characters left is legal. We already
+            // know it's a small capacity island with no pilings, so the
+            // character limit will always be 1 here
+            if (numberOfCharactersAfterMovement > 1) {
+                throw new Error(
+                    'Movement results in too many characters on an island.',
+                );
+            }
+        }
+
+        // each movement must be for a different character
+        for (const movement of movementSet) {
+            const fromIsland = findIsland(game, movement.fromIslandNumber);
+
+            // can't move from an island that doesn't exist
+            if (!fromIsland) {
+                throw new Error(
+                    "Cannot move from an island that doesn't exist.",
+                );
+            }
+            // find the number of potential characters on the from island that
+            // this movement could refer to
+            const numberOfMatchingCharacters = fromIsland.characters.filter(
+                (character) => {
+                    return CharacterOperations.equals(
+                        character,
+                        movement.character,
+                    );
+                },
+            ).length;
+
+            // find the number of movements in the movement set that refer to
+            // this type of character on this from island
+            const numberOfMovesUsingThisTypeOfCharacter = movementSet.filter(
+                (otherMovement) => {
+                    return (
+                        CharacterOperations.equals(
+                            otherMovement.character,
+                            movement.character,
+                        ) &&
+                        otherMovement.fromIslandNumber ===
+                            movement.fromIslandNumber
+                    );
+                },
+            ).length;
+
+            // if there are not enough of the type of character in question on
+            // the from island in question, then at least two of the movements
+            // in the movement set are trying to refer to the same character
+            if (
+                numberOfMovesUsingThisTypeOfCharacter >
+                numberOfMatchingCharacters
+            ) {
+                throw new Error(
+                    'Two movements tried to move the same character.',
+                );
+            }
+        }
+
+        // the total movement must be at least 1 and no more than 3
+        const totalMovement = computeMovementSteps(game.islands, movementSet);
+        if (totalMovement < 1 || totalMovement > 3) {
+            throw new Error('Too many or not enough movement points spent.');
+        }
+
+        // all checks passed
+
+        // make the moves
+        movementSet.forEach((movement) => {
+            console.log(
+                `Player ${playerDesignator} moves their ${
+                    movement.character.strength
+                }-strength ${
+                    movement.character.tortoise ? 'tortoise' : 'character'
+                } from island ${movement.fromIslandNumber} to island ${
+                    movement.toIslandNumber
+                }.`,
+            );
+            IslandOperations.removeCharacter(
+                findIsland(game, movement.fromIslandNumber) as IslandSerialized,
+                movement.character,
+            );
+            // reset tortoise and reclaim card if necessary
+            if (movement.character.tortoise) {
+                movement.character.tortoise = false;
+                PlayerOperations.reclaim(
+                    game.players[movement.character.playerDesignator],
+                    CardType.TORTOISE,
+                );
+            }
+            IslandOperations.addCharacter(
+                findIsland(game, movement.toIslandNumber) as IslandSerialized,
+                movement.character,
+            );
+        });
+    };
+
     /**
      * Attempts to take the given action on the given game.
      *
@@ -717,7 +920,8 @@ export namespace GameOperations {
                 break;
             case GameActionType.MOVEMENT_SET:
                 checkGameStateAndPlayer(GameState.AWAIT_MOVEMENT_SET);
-                throw new Error('TODO: unimplemented game action');
+                handleMovementAction(game, playerDesignator, gameAction.data);
+                break;
             case GameActionType.NET_TARGET:
                 checkGameStateAndPlayer(GameState.AWAIT_NET_TARGET);
                 throw new Error('TODO: unimplemented game action');
