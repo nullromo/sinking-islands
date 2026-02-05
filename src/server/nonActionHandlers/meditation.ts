fdfd7d78066651b5e49e8a915985b2ef01fae77a
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-02-05 13:14:43 -0800

    handle meditation

--- src/server/nonActionHandlers/meditation.ts
+++ src/server/nonActionHandlers/meditation.ts
@@ -1,5 +1,11 @@
 import type { GameSerialized } from '../../commonTypes';
+import { GameOperations } from '../gameObjects/gameOperations';
+import { PlayerOperations } from '../gameObjects/playerOperations';
 
 export const handleMeditation = (game: GameSerialized) => {
-    throw new Error('handle meditation todo');
+    const card = GameOperations.getCurrentlyResolvingCard(game);
+    const player = card.playerDesignator;
+
+    console.log(`Player ${player} meditates.`);
+    PlayerOperations.reshuffle(game.players[player]);
 };
