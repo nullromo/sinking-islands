e552deb5ea329a1458e2dde9578a7886db504b02
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-02-05 13:16:04 -0800

    handle prayer

--- src/server/nonActionHandlers/prayer.ts
+++ src/server/nonActionHandlers/prayer.ts
@@ -1,5 +1,30 @@
-import type { GameSerialized } from '../../commonTypes';
+import { IslandType, type GameSerialized } from '../../commonTypes';
+import { GameOperations } from '../gameObjects/gameOperations';
+import { PlayerOperations } from '../gameObjects/playerOperations';
 
 export const handlePrayer = (game: GameSerialized) => {
-    throw new Error('handle prayer todo');
+    const card = GameOperations.getCurrentlyResolvingCard(game);
+    const player = card.playerDesignator;
+
+    // determine the prayer value
+    const cardsToDraw = game.islands
+        .filter((island) => {
+            return island.islandType === IslandType.SACRED;
+        })
+        .reduce((total, island) => {
+            return (
+                total +
+                island.characters.filter((character) => {
+                    return character.playerDesignator === player;
+                }).length
+            );
+        }, 0);
+
+    // draw the cards
+    console.log(
+        `Player ${player} prays for ${cardsToDraw} card${
+            cardsToDraw === 1 ? '' : 's'
+        }.`,
+    );
+    PlayerOperations.draw(game.players[player], cardsToDraw);
 };
