694b7428058c59a6ef1981bc3cb0c54b3fa97ed4
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-02-13 16:18:47 -0800

    add emoji file

---
+++ src/emoji.ts
@@ -0,0 +1,12 @@
+// TODO: emoji should be placeholders. Use real icons later
+export const Emoji = {
+    alone: '👤',
+    house: '🏠',
+    net: '🥅',
+    person: '🧍',
+    pray: '🙏🏼',
+    storm: '⛈️',
+    together: '👥',
+    tortoise: '🐢',
+    volcano: '🌋',
+};
