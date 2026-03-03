aa28a6d85f7bd5c7df50e8ce798a299db3dc932f
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-03-02 19:05:43 -0800

    clean up tutorial more

--- src/tutorial/tutorial.tsx
+++ src/tutorial/tutorial.tsx
@@ -7,10 +7,9 @@ import type { NumberMap } from '../maps';
 import { PageRoutes } from '../pageRoutes';
 import { GameOperations } from '../server/gameObjects/gameOperations';
 
-const TutorialPage = (props: {
-    readonly title: string;
-    readonly content: React.JSX.Element;
-}) => {
+const TutorialPage = (
+    props: { readonly title: string } & React.PropsWithChildren,
+) => {
     return (
         <div
             style={{
@@ -24,7 +23,7 @@ const TutorialPage = (props: {
             }}
         >
             <h1 style={{ margin: 0 }}>{props.title}</h1>
-            {props.content}
+            {props.children}
         </div>
     );
 };
@@ -32,129 +31,108 @@ const TutorialPage = (props: {
 /* eslint-disable react/jsx-key */
 /* eslint-disable @eslint-react/no-missing-key */
 const pages: NumberMap<React.JSX.Element> & { length: number } = [
-    <TutorialPage
-        content={
-            <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    flexGrow: 1,
-                    justifyContent: 'space-evenly',
-                    padding: '12px 30%',
-                    textAlign: 'center',
-                }}
-            >
-                <div>
-                    <em>
-                        {
-                            "The gods are displeased! This petty human war has gone on far too long, and it's time for it to end!"
-                        }
-                    </em>
-                </div>
-                <div>
+    <TutorialPage title='Introduction'>
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                flexGrow: 1,
+                justifyContent: 'space-evenly',
+                padding: '12px 30%',
+                textAlign: 'center',
+            }}
+        >
+            <div>
+                <em>
                     {
-                        "In Sinking Islands, battle ensues on an ever-shrinking archipelago. The gods have begun to sink the islands one at a time, and they won't stop until one army eliminates the other."
+                        "The gods are displeased! This petty human war has gone on far too long, and it's time for it to end!"
+                    }
+                </em>
+            </div>
+            <div>
+                {
+                    "In Sinking Islands, battle ensues on an ever-shrinking archipelago. The gods have begun to sink the islands one at a time, and they won't stop until one army eliminates the other."
+                }
+            </div>
+            <div>
+                <em>
+                    {
+                        'Fight for survival, lest everyone sink into the stormy sea!'
+                    }
+                </em>
+            </div>
+        </div>
+    </TutorialPage>,
+    <TutorialPage title='The Archipelago'>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                width: '100%',
+            }}
+        >
+            <div
+                style={{ alignItems: 'center', display: 'flex', width: '80%' }}
+            >
+                <CircularContainer
+                    items={GameOperations.create().islands}
+                    renderItem={(island, itemWidth) => {
+                        return (
+                            <div
+                                key={island.islandNumber}
+                                style={{ height: '100%', width: '100%' }}
+                            >
+                                <div
+                                    style={{
+                                        alignItems: 'flex-end',
+                                        backgroundImage: getIslandImage(
+                                            island.islandNumber,
+                                        ),
+                                        backgroundPosition: 'center',
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundSize: 'cover',
+                                        border: `${(itemWidth * 3) / 140}px solid ${getIslandColors(island).island}`,
+                                        display: 'flex',
+                                        fontSize: '18px',
+                                        height: '100%',
+                                        position: 'relative',
+                                        width: '100%',
+                                    }}
+                                />
+                            </div>
+                        );
+                    }}
+                />
+                <div style={{ padding: '0 80px' }}>
+                    {
+                        'Welcome to the Archipelago. This ring of 16 islands is all the ground your people have to stand on. The islands begin the game in a random order.'
                     }
                 </div>
-                <div>
-                    <em>
-                        {
-                            'Fight for survival, lest everyone sink into the stormy sea!'
-                        }
-                    </em>
-                </div>
             </div>
-        }
-        title='Introduction'
-    />,
-    <TutorialPage
-        content={
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    width: '100%',
-                }}
-            >
-                <div
-                    style={{
-                        alignItems: 'center',
-                        display: 'flex',
-                        width: '80%',
-                    }}
-                >
-                    <CircularContainer
-                        items={GameOperations.create().islands}
-                        renderItem={(island, itemWidth) => {
-                            return (
-                                <div
-                                    key={island.islandNumber}
-                                    style={{ height: '100%', width: '100%' }}
-                                >
-                                    <div
-                                        style={{
-                                            alignItems: 'flex-end',
-                                            backgroundImage: getIslandImage(
-                                                island.islandNumber,
-                                            ),
-                                            backgroundPosition: 'center',
-                                            backgroundRepeat: 'no-repeat',
-                                            backgroundSize: 'cover',
-                                            border: `${(itemWidth * 3) / 140}px solid ${getIslandColors(island).island}`,
-                                            display: 'flex',
-                                            fontSize: '18px',
-                                            height: '100%',
-                                            position: 'relative',
-                                            width: '100%',
-                                        }}
-                                    />
-                                </div>
-                            );
-                        }}
-                    />
-                    <div style={{ padding: '0 80px' }}>
-                        {
-                            'Welcome to the Archipelago. This ring of 16 islands is all the ground your people have to stand on. The islands begin the game in a random order.'
-                        }
-                    </div>
-                </div>
-            </div>
-        }
-        title='The Archipelago'
-    />,
-    <div>
-        <h1>Islands</h1>
-    </div>,
-    <div>
-        <h1>Characters</h1>
-    </div>,
-    <div>
-        <h1>Rising Waters</h1>
-    </div>,
-    <div>
-        <h1>Archipelago Overview</h1>
-    </div>,
-    <div>
-        <h1>Round Structure</h1>
-    </div>,
-    <div>
-        <h1>Action Order Track</h1>
-    </div>,
-    <div>
-        <h1>Cards</h1>
-    </div>,
-    <div>
-        <h1>Deck Composition</h1>
-    </div>,
-    <div>
-        <h1>Card Details</h1>
-    </div>,
-    <div>
-        <h1>Game End</h1>
-    </div>,
+        </div>
+    </TutorialPage>,
+    <TutorialPage title='Islands'>TODO</TutorialPage>,
+    <TutorialPage title='Characters'>TODO</TutorialPage>,
+    <TutorialPage title='Rising Waters'>TODO</TutorialPage>,
+    <TutorialPage title='Archipelago Overview'>TODO</TutorialPage>,
+    <TutorialPage title='Round Structure'>TODO</TutorialPage>,
+    <TutorialPage title='Action Order Track'>TODO</TutorialPage>,
+    <TutorialPage title='Cards'>TODO</TutorialPage>,
+    <TutorialPage title='Deck Composition'>TODO</TutorialPage>,
+    <TutorialPage title='Card Details'>TODO</TutorialPage>,
+    <TutorialPage title='Game End'>TODO</TutorialPage>,
 ];
 /* eslint-enable react/jsx-key */
 /* eslint-enable @eslint-react/no-missing-key */
+
+const buttonStyle: React.CSSProperties = {
+    background: 'black',
+    borderRadius: '4px',
+    color: 'white',
+    cursor: 'pointer',
+    fontSize: '16pt',
+    padding: '0 12px',
+};
 
 export const Tutorial = () => {
     const navigate = useNavigate();
@@ -184,19 +162,9 @@ export const Tutorial = () => {
                     width: '100%',
                 }}
             >
-                <div style={{ position: 'absolute' }}>
+                <div style={{ margin: '12px', position: 'absolute' }}>
                     <Link to={PageRoutes.DASHBOARD}>
-                        <button
-                            style={{
-                                background: 'black',
-                                borderRadius: '4px',
-                                color: 'white',
-                                fontSize: '16pt',
-                                margin: '12px',
-                                padding: '0 12px',
-                            }}
-                            type='button'
-                        >
+                        <button style={buttonStyle} type='button'>
                             Home
                         </button>
                     </Link>
@@ -212,12 +180,9 @@ export const Tutorial = () => {
                     <button
                         disabled={pageID <= 1}
                         style={{
+                            ...buttonStyle,
                             background: pageID <= 1 ? 'gray' : 'black',
-                            borderRadius: '4px',
-                            color: 'white',
                             cursor: pageID <= 1 ? 'default' : 'pointer',
-                            fontSize: '16pt',
-                            padding: '0 12px',
                         }}
                         type='button'
                         onClick={() => {
@@ -261,21 +226,17 @@ export const Tutorial = () => {
                             );
                         })}
                     {pageID === pages.length ? (
-                        <Link to={PageRoutes.DASHBOARD}>
-                            <button style={{ cursor: 'pointer' }} type='button'>
+                        <Link
+                            style={{ display: 'contents' }}
+                            to={PageRoutes.DASHBOARD}
+                        >
+                            <button style={buttonStyle} type='button'>
                                 Home
                             </button>
                         </Link>
                     ) : (
                         <button
-                            style={{
-                                background: 'black',
-                                borderRadius: '4px',
-                                color: 'white',
-                                cursor: 'pointer',
-                                fontSize: '16pt',
-                                padding: '0 12px',
-                            }}
+                            style={buttonStyle}
                             type='button'
                             onClick={() => {
                                 setPageID((pageID) => {
